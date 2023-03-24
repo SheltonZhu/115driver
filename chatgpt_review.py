da2009798275922567af9a1828f134dea3947210
sheltonzhu <498220739@qq.com>
2023-03-24 21:36:33 +0800

    Update chatgpt_review.py

--- chatgpt_review.py
+++ chatgpt_review.py
@@ -8,7 +8,7 @@ def get_chatgpt_review(api_token, pr_number):
       'Authorization': 'Bearer {0}'.format(api_token),
       'Content-Type': 'application/json'
     }
-    
+
     data = {
         'type': 'pull_request',
         'number': pr_number,
@@ -28,9 +28,10 @@ def post_to_comment(comment_text, pr_number, github_token):
 
     requests.post(url, headers=headers, json=payload)
 
-pr_number = int(sys.argv[1])
-github_token = sys.argv[2]
-chatgpt_api_token = sys.argv[3]
+# Retrieve inputs and environment variables
+pr_number = int(os.environ['INPUT_PR_NUMBER'])
+github_token = os.environ['GITHUB_TOKEN']
+chatgpt_api_token = os.environ['CHATGPT_API_TOKEN']
 
 review_result = get_chatgpt_review(chatgpt_api_token, pr_number)
 post_to_comment(review_result['data']['comment'], pr_number, github_token)
